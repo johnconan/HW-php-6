d4291539becff079e02aecf21eb75d5eb133aec4
johnconan <johnconan@mail.ru>
2018-07-05 13:40:56 +0300

    fixed cookies

--- login.php
+++ login.php
@@ -1,13 +1,17 @@
 <?php 
 require('config.php');
 require('functions/login-functions.php');
+require('database.php');
+$link = db_connect();
 
 if ( isset($_POST['enter']) ) {
-	$userName = 'admin';
-	$userPassword = '123456';
+	$query = "SELECT * FROM `user`";
+	if ( $result = mysqli_query($link, $query) ) {
+		$user = mysqli_fetch_array($result);
+	}
 
-	if ( $_POST['login'] == $userName ) {
-		if ( $_POST['password'] == $userPassword ) {
+	if ( $_POST['login'] == $user['userLogin'] ) {
+		if ( $_POST['password'] == $user['userPassword'] ) {
 
 			$_SESSION['user'] = 'admin';
 			header('Location: ' . HOST . 'index.php');
